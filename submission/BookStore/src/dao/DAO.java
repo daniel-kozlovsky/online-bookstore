00d4be0f142627c5f150958e0ef45c546aec0360
derevi <derevi@8350fx>
2021-02-26 21:17:16 -0500

    DAO interface completed

--- submission/BookStore/src/dao/DAO.java
+++ submission/BookStore/src/dao/DAO.java
@@ -1,12 +1,15 @@
 package dao;
 
+import java.util.List;
+import java.util.Map;
+
 import dao.accessors.queries.Query;
 
-public interface DAO {
-	public String[] getAttributeLabels();
-	public Query newQuery();
-	public Query newUpdate();
-	public void retrieveQueryResults();
-	
-
+public interface DAO<T> {
+	T getById(T t);
+	T getById(String id);	
+	Query newQueryRequest();	
+	List<T> retrieveQueryResults();	
+	Map<String,T> retrieveQueryResultsMap();	
+	String retrieveQueryResultsAsJson();
 }
--- submission/BookStore/src/dao/products/ProductDAO.java
+++ submission/BookStore/src/dao/products/ProductDAO.java
@@ -1,5 +1,54 @@
 package dao.products;
 
-public class ProductDAO {
+import java.util.List;
+import java.util.Map;
 
+import dao.accessors.queries.Query;
+
+public class ProductDAO {
+	//by id
+	//flexible
+	//
+	//
+	
+	ProductDAO(){
+		
+	}
+	
+	
+	Product getById(Product product) {
+		return null;
+	}
+	
+	Product getById(String id) {
+		return null;
+	}
+
+
+	Query query;
+	
+	
+	Query newQueryRequest() {
+		return query;
+	}
+	
+	List<Product> retrieveQueryResults() {
+		return null;
+	}
+	
+	
+	
+	Map<String,Product> retrieveQueryResultsMap() {
+		return null;
+	}
+	
+	
+	List<Product> retrieveQueryResultsAsJson() {
+		return null;
+	}
+	
+	
+	
+	
+	
 }
