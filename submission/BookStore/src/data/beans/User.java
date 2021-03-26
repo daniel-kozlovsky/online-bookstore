80b6cb5f4bbee081823212a3593de5ba1dad64f8
derevi <derevi@8350fx>
2021-03-26 16:30:15 -0400

    added user bean

---
+++ submission/BookStore/src/data/beans/User.java
@@ -0,0 +1,30 @@
+package data.beans;
+
+public class User extends IdObject{
+	
+//	User(User user){
+//		this.id=user.id;
+//	}
+//	
+//	User(Id id){
+//		this.id=id;
+//	}
+//	
+//	User(Visitor visitor){
+//		this.id=visitor.id;
+//	}
+//	
+//	User(Customer customer){
+//		this.id=customer.id;
+//	}
+
+	String userType;
+	
+	@Override
+	public String toJson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
