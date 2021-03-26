9665f3e481cdb8af272a77ab4473af3f4dcb35fa
derevi <derevi@8350fx>
2021-03-26 16:41:37 -0400

    added AttributeAccess class

---
+++ submission/BookStore/src/data/query/AttributeAccess.java
@@ -0,0 +1,12 @@
+package data.access.queries;
+
+import data.dao.BookDAO.BookStoreBookQuery;
+
+public class AttributeAccess<T extends Query> {
+	protected T query;
+	
+	public void setQuery(T query){
+		this.query=query;
+	}
+
+}
