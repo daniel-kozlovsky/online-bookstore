9311f745e111d9ffb1ddf0f175864fab86161996
derevi <derevi@8350fx>
2021-03-26 16:48:56 -0400

    added DataUpdate base class

---
+++ submission/BookStore/src/data/update/DataUpdate.java
@@ -0,0 +1,45 @@
+package data.access.mutations;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public abstract class DataUpdate {
+	protected void sendUpdateToDatabase(String updateString) {
+		
+		Connection connection= null;
+		PreparedStatement preparedStatement=null;
+		try {
+			DataSource dataSource=(DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+			connection= dataSource.getConnection();
+			preparedStatement = connection.prepareStatement(updateString);
+			preparedStatement.executeUpdate();
+		} catch (SQLException | NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if(connection!= null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(preparedStatement!=null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+	
+}
