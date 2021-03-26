d2148e5af72f88b84ca2df359b578d1dbd1e84a7
derevi <derevi@8350fx>
2021-03-26 16:33:34 -0400

    added PurchseOrderSchema class

---
+++ submission/BookStore/src/data/schema/PurchaseOrderSchema.java
@@ -0,0 +1,33 @@
+package data.schema;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import data.beans.Book;
+import data.beans.Customer;
+public class PurchaseOrderSchema extends DataSchema{
+	public static final String ID="ID";
+	public static final String BOOK="BOOK";
+	public static final String AMOUNT="AMOUNT";
+	public static final String CREATED_AT_EPOCH="CREATED_AT_EPOCH";
+	public static final String STATUS="STATUS";
+	public static final String PROCESSED_STATUS="PROCESSED";
+	public static final String SHIPPED_STATUS="SHIPPED";
+	public static final String DENIED_STATUS="DENIED";
+	public static final String DELIVERED_STATUS="DELIVERED";
+	public static final String ORDERED_STATUS="ORDERED";
+//	'PROCESSED','SHIPPED','DENIED','DELIVERED','ORDERED'
+
+	public PurchaseOrderSchema() {
+		super();
+		this.TABLE_NAME="PURCHASE_ORDER";
+		VARCHAR_ATTRIBUTE_LABELS = new String[]{ID,BOOK,AMOUNT,CREATED_AT_EPOCH};
+		ATTRIBUTE_LABELS = new String[]{ID,BOOK,AMOUNT,CREATED_AT_EPOCH};
+		WORD_ATTRIBUTE_LABELS = new String[]{STATUS};
+		NUMBER_ATTRIBUTE_LABELS = new String[]{};
+		OBJECT_ATTRIBUTE_LABELS =  new String[]{};		
+	}
+	
+}
