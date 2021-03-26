cdba630ed788177d7a7ed85386f8cba5d3d4310f
derevi <derevi@8350fx>
2021-03-26 16:48:06 -0400

    added VisitorDataFetcher base class

---
+++ submission/BookStore/src/data/fetchers/VisitorDataFetcher.java
@@ -0,0 +1,72 @@
+package data.access.queries.fetchers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import data.access.queries.Query;
+import data.beans.Book;
+import data.beans.Customer;
+import data.beans.Id;
+import data.beans.Review;
+import data.beans.Visitor;
+import data.dao.ReviewDAO;
+import data.schema.BookSchema;
+import data.schema.CustomerSchema;
+import data.schema.ReviewSchema;
+import data.schema.VisitorSchema;
+
+public class VisitorDataFetcher extends DataFetcher<Visitor>{
+	
+
+	public VisitorDataFetcher(Map<String, Set<String>> attributesToIncludInResults) {
+		super(attributesToIncludInResults);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	private VisitorSchema schema=new VisitorSchema(); 
+	
+
+
+	
+	
+	@Override
+	public Visitor resultSetToBean(ResultSet resultSet) {
+		boolean isRequestAllAttributes = this.attributesToIncludInResults.get(schema.tableName()).isEmpty();
+		String prefix = isReferenceQuery()?schema.tableName()+Query.referenceSeparator:"";	
+
+				
+		try {	
+			
+			Visitor visitor = new Visitor.Builder()
+					.withId(new Id(resultSet.getString(prefix+schema.ID)))
+					.build();
+			if (isRequestAllAttributes || attributesToIncludInResults.get(schema.tableName()).contains(schema.ID)) {
+				visitor = new Visitor.Builder(visitor).withCreatedAtEpoch(resultSet.getString(prefix + schema.ID)).build();
+			}
+
+			if (isRequestAllAttributes || attributesToIncludInResults.get(schema.tableName()).contains(schema.CREATED_AT_EPOCH)) {
+				visitor = new Visitor.Builder(visitor).withLastAccessedAtEpoch(resultSet.getString(prefix + schema.CREATED_AT_EPOCH)).build();
+			}
+
+
+			return visitor;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		System.err.println("Warning empty Visitor, since resultSet could not produce visitor object");
+		return new Visitor.Builder().build();
+		
+	}
+	
+
+
+}
