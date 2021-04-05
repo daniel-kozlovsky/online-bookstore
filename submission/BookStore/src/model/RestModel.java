f1e0731bc37dbc5a9fd1ec44e1a241025add09f4
jesseyh <jessey.harry.23@gmail.com>
2021-04-05 00:45:00 -0400

    cleaning up before rebase

--- submission/BookStore/src/model/RestModel.java
+++ submission/BookStore/src/model/RestModel.java
@@ -62,16 +62,19 @@ public class RestModel {
 	}
 	
 	public String getOrdersByISBN (String prodID) throws Exception {
-		String orderString;
+		String orderString = "";
 		
+		//Retrieve the book based on it's ISBN
 		List<Book> book = this.getBookByISBN(prodID);
 		
-		
-		
+		//Would like to save the orders in this list 
 		List<PurchaseOrder> po;
 		
+		//check size of book list and return appropriate response
 		if (book.size() == 0)
 			orderString = this.create404Message("Book with ISBN: \'" + prodID + "\' Not Found");
+		if (book.size() > 1)
+			orderString = this.create403Message("Retrieved multiple books with given ISBN: \'" + prodID + "\' please providd this output to site admin: info@bookstore.ca");
 		else
 		orderString = this.orders.newQueryRequest()
 									 .includeAllAttributesInResultFromSchema()
@@ -91,7 +94,7 @@ public class RestModel {
 		if (b.size() == 0)
 			bookString = this.create404Message("Book with ISBN: \'" + prodID + "\' Not Found");
 		else if (b.size() > 1)
-			bookString = this.create403Message("Forbidden, Retrieved multiple books with given ISBN: \'" + prodID + "\' please providd this output to site admin: info@bookstore.ca");
+			bookString = this.create403Message("Retrieved multiple books with given ISBN: \'" + prodID + "\' please providd this output to site admin: info@bookstore.ca");
 		else 
 			bookString = b.get(0).toJson();
 
